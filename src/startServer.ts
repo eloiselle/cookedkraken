e02820929a0947bc7975445352d8a1cc6a7ace50
eloiselle <12175927+eloiselle@users.noreply.github.com>
2021-01-07 02:05:08 -0500

    Update startServer.ts

--- src/startServer.ts
+++ src/startServer.ts
@@ -21,7 +21,7 @@ import dotenv from "dotenv";
 import https from "https"
 dotenv.config()
 
-
+const env = process.env.NODE_ENV ?? "dev"
 const port = process.env.SERVER_PORT ?? 80;
 export const router = express.Router({ strict: true });
 export let app = express();
@@ -66,13 +66,13 @@ app = routes(app); //register the routes
 // ████▀...██...██..██.██..██...██...
 
 // Load the SSL certificate when in production
-if (process.env.NODE_ENV == "prod") {
+if (env == "prod") {
   let privateKey = fs.readFileSync('private.key', 'utf8');
   let certificate = fs.readFileSync('eloiselle_tech.crt', 'utf8');
   let credentials = { key: privateKey, cert: certificate };
 
   let httpsServer = https.createServer(credentials, app);
   httpsServer.listen(port, () => console.log(`Server running on https://eloiselle.tech${port}`));
-} else {
+} else { 
   app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
 }
